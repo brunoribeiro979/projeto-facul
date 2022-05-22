3758d5f53184bb40bf90991fae05ef46e2e678f5
Bruno Ribeiro <bruno.santos979@yahoo.com.br>
2022-05-22 13:51:21 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { dados } from './arquivo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //acima o seletor, endereço html e endereço css
 })
+
+// Classe criada com os dados dos alunos e será apresentada no app.component.html através de um loop
+// Foi criado um array com o nome dados que esta vindo do arquivo.ts
 export class AppComponent {
-  title = 'trabalho_angular';
+  title = 'Trabalho Uninter';
+  dadosAlunos = dados;
 }
---
+++ src/app/arquivo.ts
@@ -0,0 +1,39 @@
+// Array criado para listagem de alunos
+export const dados = [
+    {
+        id: '1',
+        nome: 'Bruno Ribeiro dos Santos',
+        ru: '3507738',
+        nasc: '02/11/1989',
+        curso: 'Análise e Desenvolvimento de Sistemas'
+    },
+    {
+        id: '2',
+        nome: 'Maria Paula Martins',
+        ru: '2222222',
+        nasc: '07/03/2004',
+        curso: 'Matemática'
+    },
+    {
+        id: '3',
+        nome: 'João Carlos Souza',
+        ru: '3333333',
+        nasc: '23/06/2001',
+        curso: 'Comércio Exterior'
+    },
+    {
+        id: '4',
+        nome: 'Talita Damasceno da Silva',
+        ru: '4444444',
+        nasc: '17/04/2000',
+        curso: 'Análise e Desenvolvimento de Sistemas'
+    },
+    {
+        id: '5',
+        nome: 'Renan Souza da Silva',
+        ru: '5555555',
+        nasc: '01/01/1999',
+        curso: 'Administração'
+    },
+
+]
